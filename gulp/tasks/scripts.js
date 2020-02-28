c6457c0b1e81674ea0508c75772026e304921cc2
Timur Zakirov <1234tima.g@gmail.com>
2020-02-28 12:45:08 +0200

    gamma

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -11,8 +11,8 @@ const scripts = () => {
     .src(paths.src.js)
     .pipe(plumber())
     .pipe(babel())
-    .pipe(concat('scripts.js'))
-    .pipe(mode.production(uglify()))
+    // .pipe(concat('scripts.js'))
+    // .pipe(mode.production(uglify()))
     .pipe(gulp.dest(paths.build.js));
 };
 
